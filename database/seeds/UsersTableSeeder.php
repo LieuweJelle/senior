dafe4a8b32e75e3af5f8d86191082980601e4fe7
LieuweJelle <l.j.van.der.meulen@gmail.com>
2018-04-17 16:13:48 +0200

    columns added to users table

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -5,11 +5,20 @@ use Illuminate\Database\Seeder;
 class UsersTableSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeds. Example
      *
      * @return void
      */
     public function run()
     {
+      $faker = Faker\Factory::create();
+      for($i=0; $i<20; $i++) {
+        App\User::create([
+          'name' => $faker->name,
+          'password' => $faker->password,
+          'email' => $faker->unique()->postcode,
+          
+        ]);
+      }
     }
 }
